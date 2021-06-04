20a1ee2c5441a20e2d30aa465d17ae2c56e0a054
Wellvergton <wmc.jp@outlook.com>
2021-06-04 19:37:39 -0300

    Send to the api

--- src/components/Form/Form.svelte
+++ src/components/Form/Form.svelte
@@ -31,7 +31,12 @@
     }
   ];
   
+  let npsGrade = 10;
   let showAcknowledgmentModal = false;
+  
+  function setNpsGrade(value) {
+    npsGrade = value;
+  }
   
   function showModal() {
     showAcknowledgmentModal = true;
@@ -40,11 +45,25 @@
   function hideModal() {
     showAcknowledgmentModal = false;
   }
+  
+  function send() {
+    let endpoint = `https://intense-badlands-39093.herokuapp.com/nps/add?npsGrade=${npsGrade}&likedShippingValue=${questions[0].like}&likedPrice=${questions[1].like}&likedNavigation=${questions[2].like}&likedPayment=${questions[3].like}&likedDelivery=${questions[4].like}`;
+    const xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        showModal();
+      }
+    }
+    
+    xhttp.open("POST", endpoint, true);
+    xhttp.send();
+  }
 </script>
 
 <div class="is-flex is-flex-direction-row is-justify-content-center">
   <form on:submit|preventDefault={() => {}}>
-    <Nps />
+    <Nps onSelect={setNpsGrade} />
 
     {#each questions as question}
       <Field data={question} />
@@ -55,7 +74,7 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <button class="button is-primary" on:click={showModal}>
+            <button class="button is-primary" on:click={send}>
               Enviar
             </button>
           </div>
